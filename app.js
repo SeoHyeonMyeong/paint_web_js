77eda91143f360dc2e5a6f87a4af9450b8aaa547
SeoHyeonMyeong <gusaud2199@naver.com>
2021-12-23 16:31:34 +0900

    Update: new api (change brush size)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("paint");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("control_color");
+const range = document.getElementById("range");
+const mode = document.getElementById("modeBtn");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -10,6 +12,7 @@ ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(event) {
     const x = event.offsetX;
@@ -36,6 +39,22 @@ function changeColor(event){
     ctx.strokeStyle = color;
 }
 
+function changeSize(event){
+    const size = event.target.value;
+    ctx.lineWidth = size
+}
+
+function changeMode(event){
+    if(filling){
+        filling = false;
+        mode.innerText = "FILL"
+    }
+    else {
+        filling = true;
+        mode.innerText = "PAINT"
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -43,7 +62,15 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
-console.log(colors);
 Array.from(colors).forEach(color =>
     color.addEventListener("click", changeColor)
 );
+
+
+if(range){
+    range.addEventListener("input", changeSize)
+}
+
+if(mode) {
+    mode.addEventListener("click",changeMode)
+}
