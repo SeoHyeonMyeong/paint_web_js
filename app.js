2ae3243a47097c005369c347cbc48a377150292b
SeoHyeonMyeong <gusaud2199@naver.com>
2021-12-23 17:04:11 +0900

    Update: Save Function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("control_color");
 const range = document.getElementById("range");
 const mode = document.getElementById("modeBtn");
+const save = document.getElementById("saveBtn");
 
 const INITIAL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 700;
@@ -10,6 +11,7 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+fillWhite();
 ctx.strokeStyle = INITIAL_COLOR
 ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5;
@@ -27,6 +29,11 @@ function onMouseMove(event) {
         ctx.lineTo(x,y);
         ctx.stroke();
     }
+}
+
+function fillWhite(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function startPainting(event) {
@@ -65,12 +72,25 @@ function fillCanvas(event){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function saveClick(event){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "image.png"
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", fillCanvas);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -84,4 +104,8 @@ if(range){
 
 if(mode) {
     mode.addEventListener("click",changeMode)
+}
+
+if(save){
+    save.addEventListener('click',saveClick)
 }
